f2f6cc2918c485dc59da727a0f6960d1585e2676
Jacksonts <jacksonts94@gamil.com>
2024-07-14 23:20:52 -0300

    Restaurando arquivo

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Jornada de dados")
+
+# vareiaveis
+idade = 29
+nome = "Jackson"
+#print
+print(idade)
+print(nome)
+
+# Soma
+nova_idade = idade + 1
+print(nova_idade)
+
+# Lista
+lista = [1,2,3,4,5]
+
+#Metodos
+lista_invertida = lista.reverse()
